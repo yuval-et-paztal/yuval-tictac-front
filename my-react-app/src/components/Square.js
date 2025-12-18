4639ae0cb36df91db6e538022f78f8976551cfbb
yuval aviv <yuvalaviv56@gmail.com>
2025-12-18 19:06:15 +0200

    feature: base workable frontend version

---
+++ my-react-app/src/components/Square.js
@@ -0,0 +1,12 @@
+function Square({ value, onClick, highlight }) {
+  return (
+    <button
+      className={`square ${highlight ? "highlight" : ""}`}
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default Square;
